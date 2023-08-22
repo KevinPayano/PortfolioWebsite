f0610c21ea199ec30989ef94f370c60e6cd3e960
Kevin Payano <9260846+KevinPayano@users.noreply.github.com>
2023-08-22 10:42:06 -0400

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  fontFamily: {
+			IBMPlex: ["IBM Plex Sans", "sans-serif"],
+			Roboto: ["Roboto", "sans-serif"],
+		  },
+	
+		  animation: {
+			contactGrad: "contactGrad 7s infinite",
+			'spin-slow': 'spin 5s linear infinite',
+		  },
+	
+		  keyframes: {
+			contactGrad: {
+			  "0%": {
+				transform: "translate(0px, 0px) scale(1)",
+			  },
+			  "33%": {
+				transform: "translate(20px, -32px) scale(1.1)",
+			  },
+			  "66%": {
+				transform: "translate(24px, 0px) scale(0.8)",
+			  },
+			  "100%": {
+				transform: "translate(0px, 0px) scale(1)",
+			  }
+			}
+		  },
+	
+		},
+	  },
+	  plugins: [],
+}
